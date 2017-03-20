26aea5e3b94ad55034f059eeec8042294832b389
clon1502 <nicolas1cloutier@gmail.com>
2017-03-20 15:40:44 -0400

    FFT nain

--- Logiciel & Hardware/FFT_piezo_Test/Nain/Matlab_Nain/TestCorrelation_Nain.m
+++ Logiciel & Hardware/FFT_piezo_Test/Nain/Matlab_Nain/TestCorrelation_Nain.m
@@ -1,18 +1,23 @@
-clc; clear all; close all;
+clc; 
+clear all; 
+close all;
 
-x1 = xlsread('C:\Users\gaud1910\Desktop\Bas1')
-x2 = xlsread('C:\Users\gaud1910\Desktop\Bas2')
-x3 = xlsread('C:\Users\gaud1910\Desktop\Bas3')
+sheet = 1;
+range = 'A1:B1024';
 
-x4 = xlsread('C:\Users\gaud1910\Desktop\NoTouch')
-x5 = xlsread('C:\Users\gaud1910\Desktop\Haut')
+x1 = xlsread('Chapeau1.xlsx',sheet,range);
+x2 = xlsread('Chapeau2.xlsx',sheet,range);
+x3 = xlsread('Chapeau3.xlsx',sheet,range);
 
-r1 = 1 - (sum((x1(:,2)-x3(:,2)).*(x1(:,2)-x3(:,2)))/sum(x1(:,2).^2)) %Bas1 et Bas3
-r2 = 1 - (sum((x2(:,2)-x3(:,2)).*(x2(:,2)-x3(:,2)))/sum(x2(:,2).^2)) %Bas2 et Bas3
-r3 = 1 - (sum((x2(:,2)-x1(:,2)).*(x2(:,2)-x1(:,2)))/sum(x2(:,2).^2)) %Bas1 et Bas2
-r4 = 1 - (sum((x1(:,2)-x4(:,2)).*(x1(:,2)-x4(:,2)))/sum(x1(:,2).^2)) %Bas1 et Notouch
-r5 = 1 - (sum((x2(:,2)-x4(:,2)).*(x2(:,2)-x4(:,2)))/sum(x2(:,2).^2)) %Bas2 et Notouch
-r6 = 1 - (sum((x3(:,2)-x4(:,2)).*(x3(:,2)-x4(:,2)))/sum(x3(:,2).^2)) %Bas3 et notouch
-r7 = 1 - (sum((x1(:,2)-x5(:,2)).*(x1(:,2)-x5(:,2)))/sum(x1(:,2).^2)) %Bas1 et Haut
-r8 = 1 - (sum((x2(:,2)-x5(:,2)).*(x2(:,2)-x5(:,2)))/sum(x2(:,2).^2)) %Bas2 et Haut
-r9 = 1 - (sum((x3(:,2)-x5(:,2)).*(x3(:,2)-x5(:,2)))/sum(x3(:,2).^2)) %Bas3 et Haut
+x4 = xlsread('NoTouch1.xlsx',sheet,range');
+x5 = xlsread('Barbe1.xlsx',sheet,range);
+
+r1 = 1 - (sum((x1(:,2)-x3(:,2)).*(x1(:,2)-x3(:,2)))/sum(x1(:,2).^2)) %x1 et x3
+r2 = 1 - (sum((x2(:,2)-x3(:,2)).*(x2(:,2)-x3(:,2)))/sum(x2(:,2).^2)) %x2 et x3
+r3 = 1 - (sum((x2(:,2)-x1(:,2)).*(x2(:,2)-x1(:,2)))/sum(x2(:,2).^2)) %x1 et x2
+r4 = 1 - (sum((x1(:,2)-x4(:,2)).*(x1(:,2)-x4(:,2)))/sum(x1(:,2).^2)) %x1 et x4
+r5 = 1 - (sum((x2(:,2)-x4(:,2)).*(x2(:,2)-x4(:,2)))/sum(x2(:,2).^2)) %x2 et x4
+r6 = 1 - (sum((x3(:,2)-x4(:,2)).*(x3(:,2)-x4(:,2)))/sum(x3(:,2).^2)) %x3 et x4
+r7 = 1 - (sum((x1(:,2)-x5(:,2)).*(x1(:,2)-x5(:,2)))/sum(x1(:,2).^2)) %x1 et x5
+r8 = 1 - (sum((x2(:,2)-x5(:,2)).*(x2(:,2)-x5(:,2)))/sum(x2(:,2).^2)) %x2 et x5
+r9 = 1 - (sum((x3(:,2)-x5(:,2)).*(x3(:,2)-x5(:,2)))/sum(x3(:,2).^2)) %x3 et x5
